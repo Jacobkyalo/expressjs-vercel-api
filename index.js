0bc458f34135f821e16ab40d7e042f9f68eb7491
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-12-16 16:59:50 +0300

    updated cors

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({
